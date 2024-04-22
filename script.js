ef50e0926f400d8cd8f9b28abec02e5d2865753b
Shubham Gupta <161700172+Shubham9860@users.noreply.github.com>
2024-04-22 20:15:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+menuOpenBtn.onclick = function() {
+  navLinks.style.left = "0";
+  }
+  menuCloseBtn.onclick = function() {
+  navLinks.style.left = "-100%";
+  }
+
+  htmlcssArrow.onclick = function() {
+    navLinks.classList.toggle("show1");
+   }
+   moreArrow.onclick = function() {
+    navLinks.classList.toggle("show2");
+   }
+   jsArrow.onclick = function() {
+    navLinks.classList.toggle("show3");
+   }
